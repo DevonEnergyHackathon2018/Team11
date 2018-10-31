8d10d18e8d4e4a3f78db54d3fc5bffc6c52b24f9
Paul PC <paul.poputa-clean@dvn.com>
2018-10-30 22:35:41 -0500

    teams fix for image

--- teams_helper.py
+++ teams_helper.py
@@ -3,17 +3,14 @@ import requests
 def teams_message(message, channel, colour, image=False):
     """sends a card on teams"""
     card={
-    "@context": "http://schema.org/extensions",
-    "@type": "MessageCard",
-    "themeColor": colour,
-    "title": "Automated DrillBit analysis",
-    "text": message
+        "@context": "http://schema.org/extensions",
+        "@type": "MessageCard",
+        "themeColor": colour,
+        "title": "Automated DrillBit analysis",
+        "text": message
     }
     if image:
-        card["images"]=[
-                {
-                    "image":image
-                }]
+        card['text']+=" - [Click here for image](%s)" % image
     wh_resp=requests.post(url=channel, json=card)
     if wh_resp.status_code != 200:
         print(wh_resp.text)
